985e136e5665102a787c0ca34ba3f8e699c0aea0
Admin <admin@mac.local>
2013-12-12 12:44:39 +0100

    Created a blank Master-Detail app

---
+++ MyGames/MyGames/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyGames
+//
+//  Created by Admin on 12/12/13.
+//  Copyright (c) 2013 Alexander Farber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
