bd34f614c93d1a1922057d365eac24f18991ae2f
Admin <admin@mac.local>
2013-12-26 00:11:15 +0100

    Created single view app and added authenticateLocalPlayer method

---
+++ MyGameCenter/MyGameCenter/AppDelegate.m
@@ -0,0 +1,67 @@
+#import "AppDelegate.h"
+#import <GameKit/GameKit.h>
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    [self authenticateLocalPlayer];
+    
+    // Override point for customization after application launch.
+    return YES;
+}
+							
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (void) authenticateLocalPlayer
+{
+    __weak GKLocalPlayer *localPlayer = [GKLocalPlayer localPlayer];
+    localPlayer.authenticateHandler = ^(UIViewController *viewController, NSError *error) {
+        if (viewController != nil)
+        {
+            //showAuthenticationDialogWhenReasonable: is an example method name. Create your own method that displays an authentication view when appropriate for your app.
+            //[self showAuthenticationDialogWhenReasonable: viewController];
+            [self.window.rootViewController presentViewController:viewController
+                                                         animated:YES
+                                                       completion:nil];
+        }
+        else if (localPlayer.isAuthenticated)
+        {
+            //authenticatedPlayer: is an example method name. Create your own method that is called after the loacal player is authenticated.
+            //[self authenticatedPlayer: localPlayer];
+            NSLog(@"isAuthenticated");
+        }
+        else
+        {
+            //[self disableGameCenter];
+            NSLog(@"disableGameCenter");
+        }
+    };
+}
+
+@end
