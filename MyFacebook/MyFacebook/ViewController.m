c02e36cd9fb820dfbe34c446e314b05c7aa832af
Alexander Farber <Alexander.Farber@gmail.com>
2013-12-28 13:17:16 +0100

    Try to check for basic access

--- MyFacebook/MyFacebook/ViewController.m
+++ MyFacebook/MyFacebook/ViewController.m
@@ -1,4 +1,6 @@
 #import "ViewController.h"
+#import <Social/Social.h>
+#import <Accounts/Accounts.h>
 
 @interface ViewController ()
 
@@ -9,7 +11,29 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    ACAccountStore *accountStore = [[ACAccountStore alloc] init];
+    ACAccountType *facebookAccountType = [accountStore accountTypeWithAccountTypeIdentifier: ACAccountTypeIdentifierFacebook];
+    
+    NSDictionary *options = @{
+                              ACFacebookAudienceKey : ACFacebookAudienceEveryone,
+                              ACFacebookAppIdKey : @"432298283565593",
+                              ACFacebookPermissionsKey : @[@"id",
+                                                           @"first_name",
+                                                           @"gender",
+                                                           @"location"]};
+    
+    [accountStore requestAccessToAccountsWithType:facebookAccountType options:options completion:^(BOOL granted, NSError *error)
+    {
+        if (granted)
+        {
+            NSLog(@"Basic access granted");
+        }
+        else
+        {
+            NSLog(@"Basic access denied %@", error);
+        }
+    }];
 }
 
 - (void)didReceiveMemoryWarning
