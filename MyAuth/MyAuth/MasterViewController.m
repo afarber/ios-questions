f8c1fae2af88e6eb46a2938f5b7e48c6f41da404
Admin <admin@mac.local>
2014-01-05 23:35:07 +0100

    Save more data in the _menu

--- MyAuth/MyAuth/MasterViewController.m
+++ MyAuth/MyAuth/MasterViewController.m
@@ -2,6 +2,12 @@
 
 #import "DetailViewController.h"
 
+static NSString *kLabel   = @"label";
+static NSString *kAppId   = @"app_id";
+static NSString *kSecret  = @"secret";
+static NSString *kAppUrl  = @"app_url";
+static NSString *kAuthUrl = @"auth_url";
+
 @interface MasterViewController () {
     NSDictionary *_menu;
     NSArray *_keys;
@@ -15,14 +21,44 @@
     [super awakeFromNib];
     
     _keys = @[@"FB", @"GG", @"MR", @"OK", @"VK"];
-    
+        
     _menu = @{
-               @"FB": @"Facebook",
-               @"GG": @"Google+",
-               @"MR": @"Mail.ru",
-               @"OK": @"Odnoklassniki",
-               @"VK": @"Vkontakte",
-               };
+              @"FB": @{
+                      kLabel:   @"Facebook",
+                      kAppId:   @"432298283565593",
+                      kSecret:  @"c59d4f8cc0a15a0ad4090c3405729d8e",
+                      kAppUrl:  @"https://www.facebook.com/connect/login_success.html",
+                      kAuthUrl: @"https://graph.facebook.com/oauth/authorize?",
+                      },
+              @"GG": @{
+                      kLabel:   @"Google+",
+                      //kAppId:
+                      //kSecret:
+                      kAppUrl:  @"urn:ietf:wg:oauth:2.0:oob",
+                      kAuthUrl: @"https://accounts.google.com/o/oauth2/auth?",
+                      },
+              @"MR": @{
+                      kLabel:   @"Mail.ru",
+                      kAppId:   @"715360",
+                      kSecret:  @"4260eeea98d7665edbe4baa080af894b",
+                      kAppUrl:  @"http://connect.mail.ru/oauth/success.html",
+                      kAuthUrl: @"https://connect.mail.ru/oauth/authorize?",
+                      },
+              @"OK": @{
+                      kLabel:   @"Odnoklassniki",
+                      //kAppId:
+                      //kSecret:
+                      kAppUrl:  @"http://connect.mail.ru/oauth/success.html",
+                      kAuthUrl: @"http://www.odnoklassniki.ru/oauth/authorize?",
+                      },
+              @"VK": @{
+                      kLabel:   @"VKontakte",
+                      //kAppId:
+                      //kSecret:
+                      kAppUrl:  @"http://oauth.vk.com/blank.html",
+                      kAuthUrl: @"http://oauth.vk.com/authorize?",
+                      },
+            };
     
 }
 
@@ -53,8 +89,8 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
 
     NSString *key = _keys[indexPath.row];
-    NSDate *object = _menu[key];
-    cell.textLabel.text = [object description];
+    NSString *label = _menu[key][kLabel];
+    cell.textLabel.text = label;
     return cell;
 }
 
@@ -63,8 +99,8 @@
     if ([[segue identifier] isEqualToString:@"showDetail"]) {
         NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
         NSString *key = _keys[indexPath.row];
-        NSDate *object = _menu[key];
-        [[segue destinationViewController] setDetailItem:object];
+        NSString *label = _menu[key][kLabel];
+        [[segue destinationViewController] setDetailItem:label];
     }
 }
 
