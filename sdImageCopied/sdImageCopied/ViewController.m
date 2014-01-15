388a750d38d374187b83208249e5255e9db73e89
Alexander Farber <Alexander.Farber@gmail.com>
2014-01-15 12:48:34 +0100

    Use SDWebImage by simply copying all its *.h and *.m files into the SDWebImage subdir

---
+++ sdImageCopied/sdImageCopied/ViewController.m
@@ -0,0 +1,25 @@
+#import "ViewController.h"
+#import "SDWebImage/UIImageView+WebCache.h"
+
+static NSString* const kAvatar = @"http://gravatar.com/avatar/55b3816622d935e50098bb44c17663bc.png";
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [_imageView setImageWithURL:[NSURL URLWithString:kAvatar]
+               placeholderImage:[UIImage imageNamed:@"male.png"]];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
