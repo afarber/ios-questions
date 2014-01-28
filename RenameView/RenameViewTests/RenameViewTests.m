068db912682593e921bc4d9a3f45f5114323d687
Alexander Farber <Alexander.Farber@gmail.com>
2014-01-28 11:22:49 +0100

    Create a blank Master-Detail Application for iPhone

---
+++ RenameView/RenameViewTests/RenameViewTests.m
@@ -0,0 +1,34 @@
+//
+//  RenameViewTests.m
+//  RenameViewTests
+//
+//  Created by Admin on 28/01/14.
+//  Copyright (c) 2014 Alexander Farber. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RenameViewTests : XCTestCase
+
+@end
+
+@implementation RenameViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
