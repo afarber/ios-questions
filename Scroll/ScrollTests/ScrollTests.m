b757bc808f9a8e98349301697ca868511576b20c
Alexander Farber <Alexander.Farber@gmail.com>
2014-02-17 20:37:30 +0100

    Create a blank universal app with scroll view and 2 png images

---
+++ Scroll/ScrollTests/ScrollTests.m
@@ -0,0 +1,34 @@
+//
+//  ScrollTests.m
+//  ScrollTests
+//
+//  Created by Admin on 17/02/14.
+//  Copyright (c) 2014 Alexander Farber. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ScrollTests : XCTestCase
+
+@end
+
+@implementation ScrollTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
