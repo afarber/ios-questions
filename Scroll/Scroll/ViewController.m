23ac0e17c0af56adb187b22b753c9ea7e0125eaf
Alexander Farber <Alexander.Farber@gmail.com>
2014-02-22 17:12:25 +0100

    Set the image view dimensions to 1024 x 1024 in Interface Builder

--- Scroll/Scroll/ViewController.m
+++ Scroll/Scroll/ViewController.m
@@ -9,19 +9,11 @@
     UIImage *img = [UIImage imageNamed:@"board"];
     NSLog(@"%s: (%f x %f)", __PRETTY_FUNCTION__, img.size.width, img.size.height);
     _imageView.image = img;
-    _imageView.frame = CGRectMake(0, 0, img.size.width, img.size.height);
 }
 
 - (void)viewDidLayoutSubviews
 {
     [super viewDidLayoutSubviews];
-    
-    _scrollView.contentSize = _imageView.bounds.size;
-}
-
-- (void) willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
-{
-    [super willAnimateRotationToInterfaceOrientation:toInterfaceOrientation duration:duration];
     
     _scrollView.contentSize = _imageView.bounds.size;
 }
