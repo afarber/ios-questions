31d9395a0a1e3e549a93967cebf906eb466dc2de
Alexander Farber <Alexander.Farber@gmail.com>
2014-03-04 11:14:30 +0100

    Instantiate 5 tiles

--- DragTiles/DragTiles/ViewController.m
+++ DragTiles/DragTiles/ViewController.m
@@ -8,18 +8,18 @@ static int const kNumTiles = 5;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    Tile *tile = [[[NSBundle mainBundle] loadNibNamed:@"Tile" owner:self options:nil] firstObject];
-    tile.frame = CGRectMake(100, 0, 100, 100);
-    [self.view addSubview:tile];
-    
-    UIPanGestureRecognizer *recognizer = [[UIPanGestureRecognizer alloc]
-                                          initWithTarget:self
-                                          action:@selector(dragTile:)];
-    [tile addGestureRecognizer:recognizer];
     
     for (int i = 0; i < kNumTiles; i++) {
-        // TODO: add a Tile to the VC here
+        Tile *tile = [[[NSBundle mainBundle] loadNibNamed:@"Tile"
+                                                    owner:self
+                                                  options:nil] firstObject];
+        tile.frame = CGRectMake(100, 0, 100, 100);
+        [self.view addSubview:tile];
+        
+        UIPanGestureRecognizer *recognizer = [[UIPanGestureRecognizer alloc]
+                                              initWithTarget:self
+                                              action:@selector(dragTile:)];
+        [tile addGestureRecognizer:recognizer];
     }
 }
 
