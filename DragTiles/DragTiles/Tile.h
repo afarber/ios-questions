7c1c27c8dd6cacdaea01084c7bcb491773a01e8a
Alexander Farber <Alexander.Farber@gmail.com>
2014-03-05 19:18:20 +0100

    Add 2 image views in Tile.xib

--- DragTiles/DragTiles/Tile.h
+++ DragTiles/DragTiles/Tile.h
@@ -2,7 +2,8 @@
 
 @interface Tile : UIView
 
-@property (weak, nonatomic) IBOutlet UIImageView *background;
+@property (weak, nonatomic) IBOutlet UIImageView *dragged;
+@property (weak, nonatomic) IBOutlet UIImageView *normal;
 @property (weak, nonatomic) IBOutlet UILabel *letter;
 @property (weak, nonatomic) IBOutlet UILabel *value;
 
--- DragTiles/DragTiles/Tile.m
+++ DragTiles/DragTiles/Tile.m
@@ -24,34 +24,15 @@ static UIImage* kDragged;
     int randomInteger = 1 + (int)arc4random_uniform(9);
     NSLog(@"%s: randomLetter=%@, randomInteger=%d", __PRETTY_FUNCTION__, randomLetter, randomInteger);
     
-    _background.image = kTile;
     _letter.text = randomLetter;
     _value.text = [NSString stringWithFormat:@"%d", randomInteger];
 }
 
-- (id)initWithFrame:(CGRect)frame
-{
-    self = [super initWithFrame:frame];
-    if (self) {
-        NSString* randomLetter = [kLetters substringWithRange:[kLetters rangeOfComposedCharacterSequenceAtIndex:random()%[kLetters length]]];
-        int randomInteger = (int)arc4random_uniform(10);
-        
-        NSLog(@"%s: randomLetter=%@, randomInteger=%d",
-              __PRETTY_FUNCTION__,
-              randomLetter,
-              randomInteger);
-        
-        _background.image = kTile;
-        _letter.text = randomLetter;
-        _value.text = [NSString stringWithFormat:@"%d", randomInteger];
-    }
-    return self;
-}
-
 - (void)touchesBegan:(NSSet*)touches withEvent:(UIEvent*)event
 {
-    _background.image = kDragged;
-    [_letter setFont:[UIFont systemFontOfSize:48]];
+    [_normal setHidden:YES];
+    [_dragged setHidden:NO];
+    [_letter setFont:[UIFont systemFontOfSize:60]];
     [_value setFont:[UIFont systemFontOfSize:20]];
 
     [self.superview bringSubviewToFront:self];
@@ -69,12 +50,12 @@ static UIImage* kDragged;
     self.frame = CGRectOffset(self.frame,
                               (location.x - previous.x),
                               (location.y - previous.y));
-
 }
 
 - (void)touchesEnded:(NSSet*)touches withEvent:(UIEvent*)event
 {
-    _background.image = kTile;
+    [_normal setHidden:NO];
+    [_dragged setHidden:YES];
     [_letter setFont:[UIFont systemFontOfSize:36]];
     [_value setFont:[UIFont systemFontOfSize:16]];
     
@@ -83,7 +64,8 @@ static UIImage* kDragged;
 
 - (void)touchesCancelled:(NSSet*)touches withEvent:(UIEvent*)event
 {
-    _background.image = kTile;
+    [_normal setHidden:NO];
+    [_dragged setHidden:YES];
     [_letter setFont:[UIFont systemFontOfSize:36]];
     [_value setFont:[UIFont systemFontOfSize:16]];
     
