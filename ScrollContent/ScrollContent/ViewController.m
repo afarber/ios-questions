4121308d4459b8f1fd957adb9fd723762ab80a65
Alexander Farber <Alexander.Farber@gmail.com>
2014-03-13 14:50:27 +0100

    Add zooming on pinch and double tap gestures

--- ScrollContent/ScrollContent/ViewController.m
+++ ScrollContent/ScrollContent/ViewController.m
@@ -18,13 +18,35 @@
 {
     [super viewDidLayoutSubviews];
     
+    float scale = _scrollView.frame.size.width / 1000;
+    
+    _scrollView.minimumZoomScale = scale;
+    _scrollView.maximumZoomScale = 2 * scale;
+    _scrollView.zoomScale = 2 * scale;
+    
     _scrollView.contentSize = CGSizeMake(1000, 1000);
 }
 
-- (void)didReceiveMemoryWarning
+- (UIView*)viewForZoomingInScrollView:(UIScrollView*)scrollView
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    return _imageView;
+}
+
+- (IBAction)scrollViewDoubleTapped:(UITapGestureRecognizer*)sender
+{
+    if (_scrollView.zoomScale < _scrollView.maximumZoomScale)
+        [_scrollView setZoomScale:_scrollView.maximumZoomScale animated:YES];
+    else
+        [_scrollView setZoomScale:_scrollView.minimumZoomScale animated:YES];
+    
+    NSLog(@"%s offset=%@ size=%@ min=%f zoom=%f max=%f",
+          __PRETTY_FUNCTION__,
+          NSStringFromCGPoint(_scrollView.contentOffset),
+          NSStringFromCGSize(_scrollView.contentSize),
+          _scrollView.minimumZoomScale,
+          _scrollView.zoomScale,
+          _scrollView.maximumZoomScale
+    );
 }
 
 @end
