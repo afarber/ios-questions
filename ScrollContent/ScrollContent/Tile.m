8481032aa1a6c88efc8207d8c85b7b04a604504a
Alexander Farber <Alexander.Farber@gmail.com>
2014-03-21 10:53:07 +0100

    Set letterValues in initialize method

--- ScrollContent/ScrollContent/Tile.m
+++ ScrollContent/ScrollContent/Tile.m
@@ -1,45 +1,52 @@
 #import "Tile.h"
 
-int const kTileWidth  = 45;
-int const kTileHeight = 45;
-NSString* const kTileMoved = @"TILE_MOVED";
+int const kTileWidth            = 45;
+int const kTileHeight           = 45;
+NSString* const kTileMoved      = @"TILE_MOVED";
 
 static NSString* const kLetters = @"ABCDEFGHIJKLMNOPQRSTUWVXYZ";
+static NSDictionary* letterValues;
 
 @implementation Tile
+
++ (void)initialize
+{
+    if (self != [Tile class])
+        return;
+    
+    letterValues = @{
+         @"A": @1,
+         @"B": @4,
+         @"C": @4,
+         @"D": @2,
+         @"E": @1,
+         @"F": @4,
+         @"G": @3,
+         @"H": @3,
+         @"I": @1,
+         @"J": @10,
+         @"K": @5,
+         @"L": @2,
+         @"M": @4,
+         @"N": @2,
+         @"O": @1,
+         @"P": @4,
+         @"Q": @10,
+         @"R": @1,
+         @"S": @1,
+         @"T": @1,
+         @"U": @2,
+         @"V": @5,
+         @"W": @4,
+         @"X": @8,
+         @"Y": @3,
+         @"Z": @10,
+    };
+}
 
 - (void)awakeFromNib
 {
     [super awakeFromNib];
-    
-    NSDictionary* const letterValues = @{
-                                         @"A": @1,
-                                         @"B": @4,
-                                         @"C": @4,
-                                         @"D": @2,
-                                         @"E": @1,
-                                         @"F": @4,
-                                         @"G": @3,
-                                         @"H": @3,
-                                         @"I": @1,
-                                         @"J": @10,
-                                         @"K": @5,
-                                         @"L": @2,
-                                         @"M": @4,
-                                         @"N": @2,
-                                         @"O": @1,
-                                         @"P": @4,
-                                         @"Q": @10,
-                                         @"R": @1,
-                                         @"S": @1,
-                                         @"T": @1,
-                                         @"U": @2,
-                                         @"V": @5,
-                                         @"W": @4,
-                                         @"X": @8,
-                                         @"Y": @3,
-                                         @"Z": @10,
-                                         };
     
     NSString* randomLetter = [kLetters substringWithRange:[kLetters rangeOfComposedCharacterSequenceAtIndex:arc4random_uniform(kLetters.length)]];
     int letterValue = [letterValues[randomLetter] integerValue];
