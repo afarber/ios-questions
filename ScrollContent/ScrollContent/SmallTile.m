e5130b028475d197edfdd0ef4dc5b15f066b5f1b
Alexander Farber <Alexander.Farber@gmail.com>
2014-04-16 21:03:56 +0200

    Check more cells while searching for free cells at the game board

--- ScrollContent/ScrollContent/SmallTile.m
+++ ScrollContent/ScrollContent/SmallTile.m
@@ -50,6 +50,10 @@ static NSArray* spiral;
                @[@0, @1],
                @[@-1, @0],
                @[@0, @-1],
+               @[@1, @1],
+               @[@-1, @1],
+               @[@-1, @-1],
+               @[@1, @-1],
     ];
     
     grid = [[NSMutableArray alloc] init];
