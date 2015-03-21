6e116a5c5476d89c62241b200dadff94afcee738
Alexander Farber <Alexander.Farber@gmail.com>
2015-03-21 22:31:27 +0100

    Added MySegue project with 2 VCs and a custom button

---
+++ MySegue/MySegue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySegue
+//
+//  Created by Alexander Farber on 21.03.15.
+//  Copyright (c) 2015 Alexander Farber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MySegue/MySegue/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  MySegue
+//
+//  Created by Alexander Farber on 21.03.15.
+//  Copyright (c) 2015 Alexander Farber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
