b14dcc501410ecf89b97d4f7f1c00ed4fa679d1b
Alexander Farber <farber72@outlook.de>
2021-06-18 20:14:20 +0200

    Extract language string from observeValue params

--- Tops1/Tops1/ViewModels/TopViewModel.swift
+++ Tops1/Tops1/ViewModels/TopViewModel.swift
@@ -10,6 +10,7 @@ import Combine
 import CoreData
 
 class TopViewModel: NSObject, ObservableObject {
+
     let urls = [
         "en" : URL(string: "https://wordsbyfarber.com/ws/top"),
         "de" : URL(string: "https://wortefarbers.de/ws/top"),
@@ -24,7 +25,8 @@ class TopViewModel: NSObject, ObservableObject {
         super.init()
         UserDefaults.standard.addObserver(self,
                                           forKeyPath: "language",
-                                          options: NSKeyValueObservingOptions.new, context: nil)
+                                          options: NSKeyValueObservingOptions.new,
+                                          context: nil)
 
         let language = UserDefaults.standard.string(forKey: "language") ?? "en"
         updateTopEntities(language: language)
@@ -39,15 +41,17 @@ class TopViewModel: NSObject, ObservableObject {
                       of object: Any?,
                       change: [NSKeyValueChangeKey : Any]?,
                       context: UnsafeMutableRawPointer?) {
-        guard keyPath == "language" else { return }
-        guard change?.count == 2 else { return }
-        print("observeValue language=\(change["new"].value)")
-        // How to get language here from the params?
-        //updateTopEntities(language: language)
-        //fetchTopModels(language: language)
+        guard keyPath == "language",
+              let change = change,
+              let language = change[.newKey] as? String else {
+            return
+        }
+        updateTopEntities(language: language)
+        fetchTopModels(language: language)
     }
 
     func updateTopEntities(language:String) {
+        print("updateTopEntities language=\(language)")
         let viewContext = PersistenceController.shared.container.viewContext
         let request = NSFetchRequest<TopEntity>(entityName: "TopEntity")
         request.sortDescriptors = [ NSSortDescriptor(keyPath: \TopEntity.elo, ascending: false) ]
@@ -61,6 +65,7 @@ class TopViewModel: NSObject, ObservableObject {
     }
     
     func fetchTopModels(language:String) {
+        print("fetchTopModels language=\(language)")
         // as? means "this might be nil"
         guard let url = urls[language] as? URL else { return }
         
@@ -68,7 +73,7 @@ class TopViewModel: NSObject, ObservableObject {
             .tryMap(handleOutput)
             .decode(type: TopResponse.self, decoder: JSONDecoder())
             .sink { completion in
-                print(completion)
+                print("fetchTopModels completion=\(completion)")
             } receiveValue: { fetchedTops in
                 PersistenceController.shared.container.performBackgroundTask { backgroundContext in
                     backgroundContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
