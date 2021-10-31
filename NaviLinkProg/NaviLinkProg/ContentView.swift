8076c396b50d13015ccc5960852455a90cce8f1c
Alexander Farber <farber72@outlook.de>
2021-10-31 20:47:59 +0100

    NavigationView was missing

--- NaviLinkProg/NaviLinkProg/ContentView.swift
+++ NaviLinkProg/NaviLinkProg/ContentView.swift
@@ -11,24 +11,25 @@ struct ContentView: View {
     @StateObject private var vm:GamesViewModel = GamesViewModel()
 
     var body: some View {
-        VStack {
-            List {
-                ForEach(vm.currentGames, id: \.self) { gameNumber in
-                    NavigationLink(destination: GameView(gameNumber: gameNumber)) {
-                        Text("Game #\(gameNumber)")
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(vm.currentGames, id: \.self) { gameNumber in
+                        NavigationLink(destination: GameView(gameNumber: gameNumber)) {
+                            Text("Game #\(gameNumber)")
+                        }
                     }
                 }
-            }
+                Button(
+                    action: { vm.updateCurrentGames() },
+                    label: { Text("Update games") }
+                ).padding(4)
 
-            Button(
-                action: { vm.updateCurrentGames() },
-                label: { Text("Update games") }
-            ).padding(4)
-
-            Button(
-                action: { vm.updateDisplayedGame() },
-                label: { Text("Join a random game") }
-            ).padding(4)
+                Button(
+                    action: { vm.updateDisplayedGame() },
+                    label: { Text("Join a random game") }
+                ).padding(4)
+          }.navigationBarTitle("Select a game")
        }
     }
 }
